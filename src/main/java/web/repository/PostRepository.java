982f90b12f89decf8c31cf2951f993cf467e3af9
Maxim-Mitrenko <klevaya.farm@gmail.com>
2022-07-27 12:45:24 +0300

    Update repository

--- src/main/java/web/repository/PostRepository.java
+++ src/main/java/web/repository/PostRepository.java
@@ -4,47 +4,43 @@ import org.springframework.stereotype.Repository;
 import web.exception.NotFoundException;
 import web.model.Post;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
-import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @Repository
 public class PostRepository implements RepositoryInterface {
 
-    private final List<Post> posts = new CopyOnWriteArrayList<>();
+    private final Map<Long, Post> posts = new ConcurrentHashMap<>();
     private final AtomicInteger size = new AtomicInteger();
 
     @Override
     public List<Post> all() {
-        return posts;
+        return new ArrayList<>(posts.values());
     }
 
     @Override
     public Optional<Post> getById(long id) {
-        return posts.stream()
-                .filter(x -> x.getId() == id)
-                .findAny();
+        return Optional.ofNullable(posts.get(id));
     }
 
     @Override
     public Post save(Post post) {
         if (post.getId() == 0) {
             post.setId(size.addAndGet(1));
-            posts.add(post);
         } else {
-            final var found = getById(post.getId()).orElseThrow(NotFoundException::new);
-            final var index = posts.indexOf(post);
-            posts.remove(found);
-            posts.add(index, post);
+            getById(post.getId()).orElseThrow(NotFoundException::new);
         }
+        posts.put(post.getId(), post);
         return post;
     }
 
     @Override
     public void removeById(long id) {
-        Post post = getById(id).orElseThrow(NotFoundException::new);
-        posts.remove(post);
-
+        var result = posts.remove(id);
+        if (result == null) throw new NotFoundException();
     }
 }
